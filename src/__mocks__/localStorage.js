f6222ed64e0dabd72b8b33d5e826db92c61eb0bb
elisepinot <elise.pinot@live.fr>
2024-04-25 15:29:26 +0200

    Ajout commentaire

--- src/__mocks__/localStorage.js
+++ src/__mocks__/localStorage.js
@@ -1,19 +1,19 @@
-export const localStorageMock = (function() {
-  let store = {};
+export const localStorageMock = (function () {
+  let store = {}
   return {
-    getItem: function(key) {
+    getItem: function (key) {
       return JSON.stringify(store[key])
     },
-    setItem: function(key, value) {
+    setItem: function (key, value) {
       store[key] = value.toString()
     },
-    clear: function() {
+    clear: function () {
       store = {}
     },
-    removeItem: function(key) {
+    removeItem: function (key) {
       delete store[key]
-    }
+    },
   }
 })()
 
-// Fournit une implémentation factice de l'interface 'localStorage' du navigateur. Cette simulation est souvent utilisée dans les tests pour éviter la dépendance au stockage réel du navigateur, permettant ainsi aux tests de s'exécuter de manière isolée et prévisible.
+// Fournit une implémentation factice de l'interface 'localStorage' du navigateur. Cette simulation est souvent utilisée dans les tests pour éviter la dépendance au stockage réel du navigateur, permettant ainsi aux tests de s'exécuter de manière isolée et prévisible. document, localStorage, etc. --> appartient au navigateur, pas à JS. Pour les tests, il faut simuler un navigateur.
